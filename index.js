fa9f2bfb514d1bf07229aa5678e7ae04bdf85c36
gracie gyimah <graciegyimah9@gmail.com>
2023-03-02 19:28:07 -0500

    fini

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const cards = ["Tom", "Sally", "Jane"];
+
+function writeCards(cards) {
+    const messages = []
+  for (let i = 0; i < cards.length; i++) 
+  {
+    messages.push(`Thank you, ${cards [i]}, for the wonderful surprise gift!`)      
+    };
+  return messages
+}
+
+function countDown() {
+let num = 10;
+while (num >= 0) {
+  console.log(num--);
+}
+}
